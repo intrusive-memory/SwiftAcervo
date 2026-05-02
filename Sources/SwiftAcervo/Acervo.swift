0923471284dba08f26994d2dfe1458af9a6e53df
Tom Stovall <119924+stovak@users.noreply.github.com>
2026-05-01 21:58:24 -0700

    chore: mark development as 0.8.5-dev
    Post-release dev marker. The -dev suffix will be stripped at the next
    ship cycle.

--- Sources/SwiftAcervo/Acervo.swift
+++ Sources/SwiftAcervo/Acervo.swift
@@ -27,7 +27,7 @@ import Foundation
 public enum Acervo {
 
   /// The current version of SwiftAcervo.
-  public static let version = "0.8.5"
+  public static let version = "0.8.5-dev"
 
   /// The name of the environment variable that gates outbound HTTP fetches.
   ///
